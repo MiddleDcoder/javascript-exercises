ec3b7ac726043cabd6eef044393385c3b5f4da73
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2025-03-29 00:15:42 +0800

    Refactored and Destructured
    Refactor the code to make it one loop than before using destructuring.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,17 @@
 const sumAll = function(firstNum, secondNum) {
 
-    let finalSum = 0;
-
     if (firstNum < 0 || secondNum < 0) return 'ERROR';
 
     if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return 'ERROR';
 
-    if (typeof firstNum == "string" || typeof secondNum == "string") return 'ERROR';
+    if(firstNum > secondNum) [firstNum, secondNum] = [secondNum, firstNum];
 
-    if (firstNum > secondNum) {
-        for (let i = secondNum; i <= firstNum; i++) {
-            finalSum += i;
-        }
-        return finalSum;
-    } else {
-        for (let i = firstNum; i <= secondNum; i++) {
-            finalSum += i;
-        }
-        return finalSum;
+    let finalSum = 0;
+
+    for (let i = firstNum; i <= secondNum; i++) {
+        finalSum += i;
     }
-
+    return finalSum;
 
 };
 
