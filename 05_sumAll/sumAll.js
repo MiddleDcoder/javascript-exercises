1dab7dfbb1bcfb6569e02663efaa2990a7d3de4c
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2025-03-28 23:30:29 +0800

    Exercise sumAll passed all test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+
+    let finalSum = 0;
+
+    if (firstNum < 0 || secondNum < 0) return 'ERROR';
+
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return 'ERROR';
+
+    if (typeof firstNum == "string" || typeof secondNum == "string") return 'ERROR';
+
+    if (firstNum > secondNum) {
+        for (let i = secondNum; i <= firstNum; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    } else {
+        for (let i = firstNum; i <= secondNum; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    }
+
 
 };
 
