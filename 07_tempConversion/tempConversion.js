84b7fc21f7eb34c710ba47b46a02016ecb09b7a2
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2025-03-29 17:29:25 +0800

    Refactored and passed all test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,16 +1,14 @@
 const convertToCelsius = function(fahrenheit) {
   // formula °C = (f - 32) * 5/9
-  let celsiusResult = (fahrenheit - 32) * 5/9;
 
-  return celsiusResult = Math.round(celsiusResult * 10) / 10;
+  return  Math.round((fahrenheit - 32) * (5/9) * 10) / 10;
   
 };
 
 const convertToFahrenheit = function(celsius) {
   // formula °F = (9/5 * c) + 32
-  let fahrenheitResult = (9/5 * celsius) + 32;
 
-  return fahrenheitResult  = Math.round(fahrenheitResult * 10) / 10;
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 
 };
 
