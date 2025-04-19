2251282f1818421c07f312fb33bfd5565cf34243
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2025-04-19 22:36:24 +0800

    Exercise: 09_palindromes - done
    Passed all test.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  // accept only this characters
+  const alphanumericalOnly = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  // clean the characters to avoid having none alphanumeric
+  const filteredStr = str
+    .toLowerCase()
+    .split("")
+    .filter((character) => {
+      return alphanumericalOnly.includes(character);
+    })
+    .join("");
+
+  // Reverse the string from filtered characters
+  const reversedStr = filteredStr.split("").reverse().join("");
+
+  // return boolean - true or false
+  return filteredStr === reversedStr;
 };
 
 // Do not edit below this line
