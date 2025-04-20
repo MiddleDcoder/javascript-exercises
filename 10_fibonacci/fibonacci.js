3d883fc03f8ce053578fa7badb5bfbd0e7a1716a
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2025-04-20 19:07:43 +0800

    Exercise: 10_fibonacci - done
    Passed all test.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let n; // store the filtered number
+  if (typeof num !== "number") {
+    n = parseInt(num);
+  } else {
+    n = num;
+  }
 
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
+  if (n <= 2) return 1;
+
+  let prev = 1; // (n - 2)
+  let curr = 1; // (n - 1)
+
+  for (let i = 3; i <= n; i++) {
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+
+  return curr;
 };
 
 // Do not edit below this line
